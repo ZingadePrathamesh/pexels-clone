37662f9f2dbfd77ecc0436e9328a98b53011acc4
Prathamesh Zingade <zingadeprathamesh12@gmail.com>
2024-02-23 22:28:04 +0530

    search bar

--- pexels-clone-main/src/components/SearchBar.jsx
+++ pexels-clone-main/src/components/SearchBar.jsx
@@ -1,5 +1,5 @@
 import "../css/searchbar.css";
-
+import { CiImageOn } from "react-icons/ci";
 
 export default function SearchBar(){
     return(
@@ -10,9 +10,10 @@ export default function SearchBar(){
             <div className="p-auto bg-white rounded-lg border-gray flex flex-row justify-center items-center w-min">
                 <form>
                     <div className="search-div">
-                        <button className="px-5 mx-2 my-2 py-2 flex flex-row items-center bg-slate-400 text-black rounded-lg text-2xl">
-                        <svg class="spacing_noMargin__Q_PsJ" viewBox="0 0 16 16" width="16" height="16"><use xmlns="#image_outline-eb5e80fa8b4b19a3114294781046ad8c"></use></svg>
-                            Photos <svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                        <button className="px-2 mx-2 my-2 py-2 flex flex-row items-center bg-gray-300 text-black rounded-lg text-2xl border-black dark:border-gray-600">
+                            <CiImageOn className="mx-3"/>
+                            Photos 
+                            <svg class="w-2.5 h-2.5 ms-2.5 mx-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
                             </svg>
                         </button>
