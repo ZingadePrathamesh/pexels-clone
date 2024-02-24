dd2a6441dacdbb1bd9334241946d353d9ad74cb4
Prathamesh Zingade <zingadeprathamesh12@gmail.com>
2024-02-24 06:33:00 +0530

    more coding

--- pexels-clone-main/src/components/SearchBar.jsx
+++ pexels-clone-main/src/components/SearchBar.jsx
@@ -3,11 +3,14 @@ import { CiImageOn } from "react-icons/ci";
 
 export default function SearchBar(){
     return(
-        <div className="container p-auto m-auto flex flex-column justify-center items-center w-min">
-            <div>
-
+        <div className="container p-auto m-auto flex flex-col justify-start items-center">
+            <div className="flex flex-col justify-start items-start">
+            <div className="my-4">
+                <h2 className="text-white text-5xl font-medium">
+                    The best free stock photos, royalty free <br/>images & videos shared by creators.
+                </h2>
             </div>
-            <div className="p-auto bg-white rounded-lg border-gray flex flex-row justify-center items-center w-min">
+            <div className="p-auto bg-white rounded-lg border-gray flex flex-row justify-center items-center w-min my-4">
                 <form>
                     <div className="search-div">
                         <button className="px-2 mx-2 my-2 py-2 flex flex-row items-center bg-gray-300 text-black rounded-lg text-2xl border-black dark:border-gray-600">
@@ -26,6 +29,19 @@ export default function SearchBar(){
                     </div>
                 </form>
             </div>
+            
+            <div className="my-4">
+                <h3 className="text-white text-2xl font-medium">
+                    Trending:
+                    <a href="#"> adventure, </a>
+                    <a href="#">relax, </a>
+                    <a href="#">hiking, </a>
+                    <a href="#">leaves, </a>
+                    <a href="#">gold</a>
+                </h3>
+            </div>
+            </div>
+            
             <div>
 
             </div>
